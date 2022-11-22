481150759135df2e201d47d015ef5049b3541a50
JacksonTheMaster <81807824+JacksonTheMaster@users.noreply.github.com>
2022-11-22 11:12:03 +0100

    Add files via upload

---
+++ Teams_regsrv.bat
@@ -0,0 +1,1 @@
+regsvr32 Microsoft.Teams.AddinLoader.dll
---
+++ teams_batch.bat
@@ -0,0 +1,1 @@
+%SystemRoot%\System32\regsvr32.exe /n /i:user %LocalAppData%\Microsoft\TeamsMeetingAddin\1.0.22062.1\x64\Microsoft.Teams.AddinLoader.dll
