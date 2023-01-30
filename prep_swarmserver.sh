82eeca0e8a550d450643f76fc6f26cee9780e179
JLangisch <81807824+JacksonTheMaster@users.noreply.github.com>
2023-01-30 19:22:22 +0100

    Update prep_swarmserver.sh

--- prep_swarmserver.sh
+++ prep_swarmserver.sh
@@ -3,7 +3,10 @@ apt-get upgrade
 apt-get autoremove
 NFS_SERVER="10.10.50.75"
 NFS_SHARE="/mnt/cache-00/cache-gdrive/jmg-hel-swarm-00"
-MOUNT_POINT="/mnt"
+MOUNT_POINT="/nfs"
+
+cd /
+mkdir /nfs
 
 # Mount NFS share persistently
 echo "$NFS_SERVER:$NFS_SHARE $MOUNT_POINT nfs defaults 0 0" | sudo tee -a /etc/fstab
