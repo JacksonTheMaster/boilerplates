1aa559c562123accebf9b18b7c877898f34314e3
JLangisch <81807824+JacksonTheMaster@users.noreply.github.com>
2023-01-30 18:02:43 +0100

    Update and rename update.sh to prep_swarmserver.sh

---
+++ prep_swarmserver.sh
@@ -0,0 +1,10 @@
+apt-get update
+apt-get upgrade
+apt-get autoremove
+NFS_SERVER="10.10.50.150"
+NFS_SHARE="/mnt/cache-00/cache-gdrive/jmg-hel-swarm-00"
+MOUNT_POINT="/mnt"
+
+# Mount NFS share persistently
+echo "$NFS_SERVER:$NFS_SHARE $MOUNT_POINT nfs defaults 0 0" | sudo tee -a /etc/fstab
+sudo mount -a
