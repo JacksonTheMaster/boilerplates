c9cada6e0a85e0c81c236803cdaca1d8dfb91d63
JLangisch <81807824+JacksonTheMaster@users.noreply.github.com>
2023-02-07 13:28:35 +0100

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,14 +1,3 @@
+sudo -i
 apt-get update
 apt-get upgrade
-apt-get install nfs-common -y
-NFS_SERVER="10.10.50.75"
-NFS_SHARE="/mnt/cache-00/cache-gdrive/jmg-hel-swarm-00"
-MOUNT_POINT="/nfs"
-
-cd /
-mkdir /nfs
-
-# Mount NFS share persistently
-echo "$NFS_SERVER:$NFS_SHARE $MOUNT_POINT nfs defaults 0 0" | sudo tee -a /etc/fstab
-sudo mount -a
-echo "Task OK"
