8f02a26b533cd2ca06e9de5ae30a2add7c93516a
JLangisch <81807824+JacksonTheMaster@users.noreply.github.com>
2023-10-12 23:40:35 +0200

    Update UserCreationCLI.ps1

--- Microsoft/ActiveDirectory/UserCreationCLI.ps1
+++ Microsoft/ActiveDirectory/UserCreationCLI.ps1
@@ -8,7 +8,11 @@
 #
 #
 
-
+---
+> ## ⚠️DEPRECATED⚠️
+> 
+> **Note**: This documentation has moved. Please refer to [the new documentation site](https://docs.jmg-it.de/), wich is still WIP!
+---
 
 
 
